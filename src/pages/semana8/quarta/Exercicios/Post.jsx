ef87e9700dae447b92ca9d5e4cb681dee4c1e1cc
Daniel Meireles <daniel@inovadora.com.br>
2021-10-24 19:22:39 -0300

    Finalizando o exercício 6 da semana 8

---
+++ src/pages/semana8/quarta/Exercicios/Post.jsx
@@ -0,0 +1,31 @@
+
+export const Post = ({ user, post }) => {
+    return (
+        <div className="post">
+            <div className="user-info">
+                <div className="user-img">
+                    <img srcSet="https://thispersondoesnotexist.com/image" />
+                </div>
+                <div className="user">
+                    <p>{user.name}</p>
+                    <p>{user.lastTimePost}</p>
+                </div>
+                <span className="material-icons">more_vert</span>
+            </div>
+            <div className="post-info">
+                <p>{post.description}</p>
+                <img srcSet={post.img} />
+            </div>
+            <div className="comments-info">
+                <div className="comments">
+                    <span className="material-icons favorite">favorite</span>
+                    <p>Commments</p>
+                    <p>Share</p>
+                </div>
+                <div className="likes">
+                    Liked by <strong>John dow & 25 others</strong>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/semana8/quarta/Exercicios/index.jsx
+++ src/pages/semana8/quarta/Exercicios/index.jsx
@@ -1,6 +1,7 @@
 import './style.css'
 import { Button } from './Button'
 import { Contato } from './Contato'
+import { Post } from './Post'
 
 const Semana8QuartaExercicios = () => {
     const component = `export const Menu = (props) => <h1>{props.title}</h1>`
@@ -22,7 +23,46 @@ const Semana8QuartaExercicios = () => {
             </div>
         )
     }`
+    const postComponent = `export const Post = ({ user, post }) => {
+        return (
+            <div className="post">
+                <div className="user-info">
+                    <div className="user-img">
+                        <img srcSet="https://thispersondoesnotexist.com/image" />
+                    </div>
+                    <div className="user">
+                        <p>{user.name}</p>
+                        <p>{user.lastTimePost}</p>
+                    </div>
+                    <span className="material-icons">more_vert</span>
+                </div>
+                <div className="post-info">
+                    <p>{post.description}</p>
+                    <img srcSet={post.img} />
+                </div>
+                <div className="comments-info">
+                    <div className="comments">
+                        <span className="material-icons favorite">favorite</span>
+                        <p>Commments</p>
+                        <p>Share</p>
+                    </div>
+                    <div className="likes">
+                        Liked by <strong>John dow & 25 others</strong>
+                    </div>
+                </div>
+            </div>
+        )
+    }`
 
+    const user = { 
+        name: "Daniel",
+        lastTimePost:"5m"
+    }
+    const post = { 
+        img: "https://i1.wp.com/mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg", 
+        description:"Lorem Ipsum dolor sit amet, consectureru",
+        comments: []
+    }
     return (
         <main className="container">
             <h1>Lista de Exercicios da Semana 8</h1>
@@ -59,6 +99,7 @@ const Semana8QuartaExercicios = () => {
                         {buttonText}
                     </pre>
                     <Button bg="red" text="Get Stated" />
+                    <Button bg="black" text="Get Stated" />
                 </li>
                 <li>
                     <h4>Ex 5 Componente de Lista de Contatos</h4>
@@ -72,6 +113,20 @@ const Semana8QuartaExercicios = () => {
                     <Contato name="João" description="João é um desenvolvedor de Software top." photo="https://thispersondoesnotexist.com/image" />
                     <Contato name="Mario" description="Mario é um desenvolvedor de Software top." photo="https://thispersondoesnotexist.com/image" />
                     <Contato name="Antonio" description="Antonio é um desenvolvedor de Software top." photo="https://thispersondoesnotexist.com/image" />
+                </li>
+                <li>
+                    <h4>Ex 6 Componente de Post de rede social</h4>
+                    <p>
+                    Resposta: O componente post contem dois objetos como propriedade uma com os dados do usuario e outra com os dados do post.
+                        Codigo do componente Post:
+                    </p>
+                    <pre>
+                        {postComponent}
+                    </pre>
+                    <Post
+                        user={user}
+                        post={post}
+                         />
                 </li>
             </ul>
         </main>
